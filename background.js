3fbe28258037fc3224102bf3bfc8d32d3e1623e8
Paige Ingram <paigeingram@Paiges-MacBook-Pro.local>
2021-01-09 15:38:38 -0800

    updates for background and manifest

---
+++ background.js
@@ -0,0 +1,12 @@
+{
+    "name"; "StuddyBuddy",
+    "version"; "1.0",
+    "description"; "Healthy Study Helper",
+    "background"; {
+      "scripts"; ["background.js"],
+      "persistent"; false
+    };
+    "manifest_version"; 2
+  }
+
+  
